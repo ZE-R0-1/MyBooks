2d8c9343e3f659b4bb29df8389e24f8f8d88aa84
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2025-03-21 14:02:20 +0900

    add LaunchScreen, Localization

--- MyBooks/Book/BookListView.swift
+++ MyBooks/Book/BookListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SwiftData
 
-enum SortOrder: String, Identifiable, CaseIterable {
+enum SortOrder: LocalizedStringResource, Identifiable, CaseIterable {
     case status, title, author
     
     var id: Self {
@@ -47,7 +47,7 @@ struct BookListView: View {
     }
 }
 
-#Preview {
+#Preview("English") {
     let preview = Preview(Book.self)
     let books = Book.sampleBooks
     let genres = Genre.sampleGenres
@@ -55,4 +55,27 @@ struct BookListView: View {
     preview.addExamples(genres)
     return BookListView()
         .modelContainer(preview.container)
+        .environment(\.locale, Locale(identifier: "en"))
+}
+
+#Preview("German") {
+    let preview = Preview(Book.self)
+    let books = Book.sampleBooks
+    let genres = Genre.sampleGenres
+    preview.addExamples(books)
+    preview.addExamples(genres)
+    return BookListView()
+        .modelContainer(preview.container)
+        .environment(\.locale, Locale(identifier: "de"))
+}
+
+#Preview("Korean") {
+    let preview = Preview(Book.self)
+    let books = Book.sampleBooks
+    let genres = Genre.sampleGenres
+    preview.addExamples(books)
+    preview.addExamples(genres)
+    return BookListView()
+        .modelContainer(preview.container)
+        .environment(\.locale, Locale(identifier: "ko"))
 }
